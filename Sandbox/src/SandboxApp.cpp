44c4932f127df54ce24f55ca3c396a0e2a4892a0
Marcell JUHÁSZ <marcell.juhasz@nng.com>
2020-06-08 01:40:05 +0200

    Setup basic Application and Entry point

---
+++ Sandbox/src/SandboxApp.cpp
@@ -0,0 +1,16 @@
+#include <Hazel.h>
+
+class Sandbox : public Hazel::Application
+{
+public:
+	Sandbox(){}
+	~Sandbox() {}
+
+private:
+
+};
+
+Hazel::Application* Hazel::CreateApplication()
+{
+	return new Sandbox();
+}
